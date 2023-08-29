1601bbb7858f94b80370cd7ec25a54c7dab9a5e4
Richard Wortham <richard@somekindofweb.dev>
2023-08-29 20:52:51 +0100

    Start commit handler

--- resources/js/Pages/Animals.vue
+++ resources/js/Pages/Animals.vue
@@ -18,7 +18,7 @@
                 <td class="p-4" v-else>{{ animal.genotype }}</td>
                 <td class="p-4" v-if="editing == animal.id"><input v-model="animal.breed"></td>
                 <td class="p-4" v-else>{{ animal.breed }}</td>
-                <td class="p-4" v-if="editing == animal.id"><button @click="editing = null">Done</button></td>
+                <td class="p-4" v-if="editing == animal.id"><button @click="commit">Done</button></td>
                 <td class="p-4" v-else @click="editing = animal.id">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
@@ -35,6 +35,11 @@ export default {
     props: {
         animals: Array,
         editing: Number
+    },
+    methods: {
+        async commit() {
+            alert('Working');
+        }
     }
 };
 
